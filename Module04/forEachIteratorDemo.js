d1dadada4455b942e30fa40f2cf196c112f4442f
sakshisshetty079 <65319741+sakshisshetty079@users.noreply.github.com>
2020-11-19 17:50:08 +0530

    Add files via upload

---
+++ Module04/forEachIteratorDemo.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachIteratorDemo extends LightningElement {
+
+    contacts = [
+        {Id:123, name:'Sakshi', email:'sakshi@gmail.com'},
+        {Id:321, name:'swathi', email:'swathi@gmail.com'},
+        {Id:213, name:'kishore', email:'kishore@gmail.com'},
+        {Id:121, name:'sanvi', email:'sanvi@gmail.com'}
+    ]
+}
